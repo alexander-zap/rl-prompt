fa0a3bec78a8d97d00a42bebee63c3cb75e71186
Alexander Zap <alexander.zap@alexanderthamm.com>
2023-10-02 01:03:51 +0200

    Extension of base_reward.py with mandatory compute_reward method

--- rlprompt/rewards/base_reward.py
+++ rlprompt/rewards/base_reward.py
@@ -2,13 +2,14 @@ from typing import Tuple, List, Dict, Any
 import torch
 import numpy as np
 from collections import defaultdict
+from abc import ABC, abstractmethod
 
 # Hardcoded
 SUPPORTED_LEFT_TO_RIGHT_LMS = ['distilgpt2', 'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl']
 SUPPORTED_MASK_LMS = ['distilroberta-base', 'roberta-base', 'roberta-large']
 
 
-class BaseReward:
+class BaseReward(ABC):
     _tokenizer: Any
     _generator: Any
 
@@ -18,8 +19,8 @@ class BaseReward:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.tokens_explored = set()
 
+        self.rewards_per_batch: List[torch.Tensor] = []
         self.input_rewards_per_batch: Dict[str, List[float]] = defaultdict(list)
-        self.rewards_per_batch = []
 
     def __call__(self, output_tokens: List[List[str]], to_tensor: bool, mode: str,
                  *args, **kwargs):
@@ -42,6 +43,7 @@ class BaseReward:
         else:
             return rewards_tensor.tolist(), rewards_log
 
+    @abstractmethod
     def forward(
             self,
             source_texts: List[str],
@@ -97,3 +99,19 @@ class BaseReward:
         prompt_tokens = output_tokens
         prompt_strings = _convert_tokens_to_string(prompt_tokens)
         return prompt_strings
+
+    @abstractmethod
+    def compute_rewards(self, prompts, source_strings, labels):
+        """
+
+        Args:
+            prompts ():
+            source_strings ():
+            labels ():
+
+        Returns:
+            rewards (List[Tensor]): List of rewards with same length as input.
+                NOTE: Each Tensor-object is 1-dimensional.
+
+        """
+        raise NotImplementedError
